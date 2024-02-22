eea67078e36e90c825b67025714370192e95ce04
DepressedPenguin <125665615+DepressedPenguin@users.noreply.github.com>
2024-02-22 11:24:08 +0100

    fix some problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import './App.css'
-import Navbar from './Components/Navbar';
-import Content from './Components/Content';
-import Langauges from './Components/Langauges';
-import Projects from './Components/Projects';
-import Contact from './Components/Contact';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Content from "./Components/Content";
+import Langauges from "./Components/Langauges";
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
 
 function App() {
-
   return (
     <>
-    <div className="App">
-      <Navbar/>
-      <Content/>
-      <Langauges/>
-      <Projects/>
-      <Contact/>
-    </div>
+      <div className="App">
+        <Navbar />
+        <Content />
+        <Langauges />
+        <Projects />
+        <Contact />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
